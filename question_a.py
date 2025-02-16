a1a60ac52a51c09a04e712f55bc6543b911f1a18
lilwoods-ux <mdogotimothy05@gmail.com>
2025-02-16 12:47:15 +0300

    question_a.py

--- question_a.py
+++ question_a.py
@@ -1,4 +1,3 @@
-name = "timothy"
-age = 20
-print("hello {}".format(name))
-print("you are {} years old".format(age))
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+print("hello {}".format(name), "you are {} years old".format(age))
